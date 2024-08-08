3fc3114c30633b6484cc3d1743eb8a75fe03886f
ChanatpakornD <csirintronsopon8@gmail.com>
2024-08-08 22:36:05 +0700

    feat: prob3

---
+++ CU_Bioinformatic_I/assignment/Translating RNA into Protein/main.py
@@ -0,0 +1,30 @@
+file = open(file="CU_Bioinformatic_I/assignment/Translating RNA into Protein/rosalind_prot.txt", mode='r')
+rna = file.read().strip()
+
+rna_codon = {"UUU" : "F", "CUU" : "L", "AUU" : "I", "GUU" : "V",
+           "UUC" : "F", "CUC" : "L", "AUC" : "I", "GUC" : "V",
+           "UUA" : "L", "CUA" : "L", "AUA" : "I", "GUA" : "V",
+           "UUG" : "L", "CUG" : "L", "AUG" : "M", "GUG" : "V",
+           "UCU" : "S", "CCU" : "P", "ACU" : "T", "GCU" : "A",
+           "UCC" : "S", "CCC" : "P", "ACC" : "T", "GCC" : "A",
+           "UCA" : "S", "CCA" : "P", "ACA" : "T", "GCA" : "A",
+           "UCG" : "S", "CCG" : "P", "ACG" : "T", "GCG" : "A",
+           "UAU" : "Y", "CAU" : "H", "AAU" : "N", "GAU" : "D",
+           "UAC" : "Y", "CAC" : "H", "AAC" : "N", "GAC" : "D",
+           "UAA" : "STOP", "CAA" : "Q", "AAA" : "K", "GAA" : "E",
+           "UAG" : "STOP", "CAG" : "Q", "AAG" : "K", "GAG" : "E",
+           "UGU" : "C", "CGU" : "R", "AGU" : "S", "GGU" : "G",
+           "UGC" : "C", "CGC" : "R", "AGC" : "S", "GGC" : "G",
+           "UGA" : "STOP", "CGA" : "R", "AGA" : "R", "GGA" : "G",
+           "UGG" : "W", "CGG" : "R", "AGG" : "R", "GGG" : "G" 
+           }
+
+protein_string = ""
+
+for i in range(0, len(rna), 3) :
+    if (rna_codon[rna[i:i+3]] == "STOP") :
+        break
+    
+    protein_string += rna_codon[rna[i:i+3]]
+
+print(protein_string) 
